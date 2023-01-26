7881f88005bee5c8b0f19179bcdb6fbe8851e93b
Milo <104236864+WindowsMeosu@users.noreply.github.com>
2023-01-26 23:40:12 +0100

    Add files via upload

---
+++ testfile.cpp
@@ -0,0 +1,8 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
+    LPSTR lpCmdLine, int nCmdShow)
+{
+    MessageBox(NULL, "lapcat is still under major testing: version 0.00", "Note", MB_OK);
+    return 0;
+}
